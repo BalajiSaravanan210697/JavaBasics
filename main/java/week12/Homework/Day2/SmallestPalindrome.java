698b8de722170fa4c29646bcb720b977d60b79ba
BalajiSaravanan210697 <balajisaravanan210697@gmail.com>
2023-09-21 14:10:28 +0530

    Week12Day1 - Changes and completed Day2

--- main/java/week12/Homework/Day2/SmallestPalindrome.java
+++ main/java/week12/Homework/Day2/SmallestPalindrome.java
@@ -20,11 +20,18 @@ public class SmallestPalindrome {
 	 * 5. Derive Pseudo code in paper (for the best chosen approach) Use ChatGPT to get approach or hints not the entire solution or complete code. USE THIS ONLY
 	 *  WHEN YOU ARE UNABLE TO GET THE APPROACH FOR A GIVEN PROBLEM AFTER APPLYING ALL WHAT YOU HAVE LEARNT AND PRACTICED + Remember how your brain is converting the test data input to its expected output!
 	 
-	 > Initialise String output and char[] to store the String Elements,left=0,right=s.length()-1,temp to swap
-	 > while loop to check the condition like left<right
-	 >  if the value at left equal to value at right then swap using temp varaible
-	 >  else assign the value left to right and swap
-	 > return the string output  
+	 > Initiate the left=0,right<s.length()-1 
+	 > char[] to store string character 
+	 > char temp to store the element
+	 > while loop to iterate the elements till left less than right
+	 > temp to store the value of left & assign value of left from  right & assign back the value of temp to right
+	 > increment left and decrement the right
+	 > while loop to iterate the string length or char array length
+	 > compare the value of the swapped char array with charat original string s 
+	 > if the character at original string and char array equals than increment the pointer 
+	 > else compare the value of value of the chartAt string and char array value 
+	 > replace with less than value 
+	 > return String of char array changed
 	 
 	 * 6.Dry run the pseudo code with all test data from step #2
 	 * 
@@ -38,36 +45,51 @@ public class SmallestPalindrome {
 	 * 
 	 * 11. Check for any gaps of code optimization (if not optimized already in Step #9)
 	 */
-@Test	
-public void test() {
+@Test
+public void validTestcase() {
 	Assert.assertEquals("efcfe", makeSmallestPalindrome("egcfe"));
-}	
-public String makeSmallestPalindrome(String s) {
-	int left = 0, right = s.length() - 1;
-	String output="";
-	char temp = ' ';
-	char[] chararray = s.toCharArray();
+}
 
+@Test
+public void validTestcase1() {
+	Assert.assertEquals("abba", makeSmallestPalindrome("abcd"));
+}
+@Test
+public void validTestcase2() {
+	Assert.assertEquals("teet", makeSmallestPalindrome("test"));
+}
+
+public char[] reversepElements(char[] ch) {
+	int left = 0, right = ch.length - 1;
+	char c = ' ';
 	while (left < right) {
-		if (chararray[left] == chararray[right]) {
-			temp = chararray[left];
-			chararray[right] = chararray[left];
-			chararray[right] = temp;
-			output+=chararray[left];
-			output+=chararray[right];
-		} else {
-			chararray[right] = chararray[left];
-			temp = chararray[left];
-			chararray[right] = chararray[left];
-			chararray[right] = temp;
-			output+=chararray[left];
-			output+=chararray[right];
-		}
+		c = ch[left];
+		ch[left] = ch[right];
+		ch[right] = c;
 		left++;
 		right--;
 	}
-	
-	return output;
-	
+	return ch;
+}
+
+public String makeSmallestPalindrome(String s) {
+	int pointer = 0, strvalue = 0, charvalue = 0;
+	char[] chararray = s.toCharArray();
+	reversepElements(chararray);
+	while (pointer < s.length()) {
+		if (s.charAt(pointer) == chararray[pointer]) {
+			pointer++;
+		} else {
+			strvalue = s.charAt(pointer) - 'a';
+			charvalue = chararray[pointer] - 'a';
+			if (strvalue < charvalue) {
+				chararray[pointer] = s.charAt(pointer);
+				pointer++;
+			} else {
+				pointer++;
+			}
+		}
+	}
+	return new String(chararray);
 }
 }

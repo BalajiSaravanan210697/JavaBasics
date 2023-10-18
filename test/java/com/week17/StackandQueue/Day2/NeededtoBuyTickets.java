c1f17347f082831402aad22f5742a30adb0e63c8
BalajiSaravanan210697 <balajisaravanan210697@gmail.com>
2023-10-18 09:13:57 +0530

    Week17 - Day2

--- test/java/com/week17/StackandQueue/Day2/NeededtoBuyTickets.java
+++ test/java/com/week17/StackandQueue/Day2/NeededtoBuyTickets.java
@@ -6,10 +6,19 @@ public class NeededtoBuyTickets {
 	/*
 	 * I/P - int[],int O/P - int
 	 * 
-	 * TC -  SC - 
+	 * TC - O(N)  SC - O(N)
       
-     Initialize empty Queue to add 
-
- 
+     Initialize empty Queue to add tickets array to Queue   // Queue = []
+     foreach loop to iterate the all array elements to the Queue  //
+     Initialize counter variable to zero                          // counter = 0  
+     while loop to iterate till the k position becomes zero       //
+     if the value present in queue is greater than zero than decrement the value and increment counter  // 
+     increment the k value                                        //
+     else increment the counter                                   //
+     return counter                                               //
 	 * */
+	
+	 public int timeRequiredToBuy(int[] tickets, int k) {
+
+	    }
 }
